1bf9427703ae022c4ab05952237e387bb119397c
Forni <and-not@mail.ru>
2023-07-10 11:56:05 +0300

    fix: организация игровых скриптов.

---
+++ Assets/Scripts/GameLevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Level
+{
+    /// <summary>
+    /// Создать.
+    /// </summary>
+    class GameInteface
+    {
+
+    }
+
+    public class GameLevelManager : MonoBehaviour
+    {
+        [SerializeField] private GameInteface inteface;
+        [SerializeField] private RoomController room;
+    }
+}
