ca24640103c498cbea3febd3f5ddb57e67fdeb11
Forni <and-not@mail.ru>
2023-08-12 13:35:41 +0300

    feat: доабвлен tsv loader для динамической загрузки локализации из интернета.

---
+++ Assets/Scripts/Utility/TSVLoader.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace RingInWater.Utility
+{
+    
+        public class TSVLoader
+        {
+            public enum Format
+            {
+                tsv=0,
+                csv
+            }
+
+            private Format format;
+            private string googleSheetUrl;
+            private string result = string.Empty;
+            public void SetUrl(string googleSheetUrl)
+            {
+                this.googleSheetUrl = googleSheetUrl;
+            }
+            public TSVLoader(string googleSheetUrl, Format format = Format.tsv)
+            {
+                SetUrl(googleSheetUrl);
+                this.format = format;
+            }
+            public void Load()
+            {
+                string format = this.format.ToString();
+
+                string gid = "0";
+
+                string end = $"/export?format={format}&gid={gid}";
+
+                googleSheetUrl += end;
+
+                using (WebClient client = new WebClient())
+                {
+                    byte[] fileData = client.DownloadData(googleSheetUrl);
+
+                    MemoryStream memStream = new MemoryStream(fileData);
+
+                    StreamReader reader = new StreamReader(memStream);
+
+                    result = reader.ReadToEnd();
+                }
+            }
+            public string GetResult()
+            {
+                return this.result;
+            }
+            public string LoadAndGetResult()
+            {
+                Load();
+                return this.result;
+            }
+        }
+}
